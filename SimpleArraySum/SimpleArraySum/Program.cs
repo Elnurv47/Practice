a7bdffea55c119c2c4aeb70d18754f64d81607a6
freetricks <elnurv47@gmail.com>
2020-09-09 21:46:36 +0400

    New Files Added

---
+++ SimpleArraySum/SimpleArraySum/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+
+namespace SimpleArraySum
+{
+    class Program
+    {
+        static int simpleArraySum(int[] arr)
+        {
+            int sum = 0;
+            for (int i = 0; i < arr.Length; i++)
+            {
+                sum += arr[i];
+            }
+            return sum;
+        }
+
+    static void Main(string[] args)
+        {
+            int n = Convert.ToInt32(Console.ReadLine());
+            int[] arr = Array.ConvertAll(Console.ReadLine().Split(' '), arrTemp => Convert.ToInt32(arrTemp));
+            Console.WriteLine(simpleArraySum(arr));
+            Console.ReadLine();
+        }
+    }
+
+}
